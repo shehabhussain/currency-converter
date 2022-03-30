4a764c8117220257e36d199f762677799da402a5
shehabhussain <85829287+shehabhussain@users.noreply.github.com>
2022-03-30 20:07:45 +0530

    updates

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -13,28 +13,28 @@ const Home = () => {
 				
 			</div>
 			<div className="social">
-				<a href="https://www.instagram.com/shehab_hussain/">
+				<a className="link" href="https://www.instagram.com/shehab_hussain/">
 					<img
 						className="icons"
 						src="./Social/ig.png"
 						alt="social"
 					/>
 				</a>
-				<a href="https://www.facebook.com/shehab.hussain.7">
+				<a className="link" href="https://www.facebook.com/shehab.hussain.7">
 					<img
 						className="icons"
 						src="./Social/fb.png"
 						alt="social"
 					/>
 				</a>
-				<a href="https://twitter.com/shehab_hussain7">
+				<a className="link" href="https://twitter.com/shehab_hussain7">
 					<img
 						className="icons"
 						src="./Social/twt.png"
 						alt="social"
 					/>
 				</a>
-				<a href="https://github.com/shehabhussain">
+				<a className="link" href="https://github.com/shehabhussain">
 					<img
 						className="icons"
 						src="./Social/github.png"
